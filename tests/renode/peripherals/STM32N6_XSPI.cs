0d1a853c04d9353d10c8dada7538368f6b154e41
changting27 <changting27@outlook.com>
2026-07-14 11:26:10 +0800

    tests: raise XSPI Renode model to L2 (ADR-025)
    Model CR.EN stick, BUSY clear, IR/CCR completion with TCF/FTF,
    and FCR clear so XSPI transfer polls finish in simulation.
    
    Signed-off-by: changting27 <changting27@outlook.com>

--- tests/renode/peripherals/STM32N6_XSPI.cs
+++ tests/renode/peripherals/STM32N6_XSPI.cs
@@ -4,7 +4,9 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 // STM32N6 XSPI (Extended SPI) model for Renode.
-// Register layout matches CMSIS XSPI_TypeDef (stm32n647xx.h).
+// L2 model: CR.EN sticks; BUSY always clear; writing IR (or CCR when
+// instruction mode requires) completes immediately with TCF; FCR clears
+// flags. Enough for driver xspi_wait_tc / xspi_wait_idle polls.
 //
 // Registers:
 //   CR   @ 0x000: Control register
@@ -38,35 +40,47 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
 
         public long Size => 0x1000;
 
+        public override void Reset()
+        {
+            base.Reset();
+            crReg = 0;
+            dcr1Reg = 0;
+            tcf = false;
+            tef = false;
+            ftf = false;
+        }
+
+        private void CompleteTransfer()
+        {
+            // Instant complete in simulation when peripheral is enabled
+            if ((crReg & 0x1) != 0)
+            {
+                tcf = true;
+                ftf = true;
+            }
+        }
+
         private void DefineRegisters()
         {
             // CR @ 0x000: Control register
-            // EN (bit 0), ABORT (bit 1), DMAEN (bit 2), TCEN (bit 3),
-            // DMM (bit 6), FSEL (bit 7), FTHRES [13:8],
-            // TEIE (bit 16), TCIE (bit 17), FTIE (bit 18),
-            // SMIE (bit 19), TOIE (bit 20), APMS (bit 22), PMM (bit 23),
-            // CSSEL (bit 24), FMODE [29:28], MSEL [31:30]
             Registers.CR.Define(this)
-                .WithValueField(0, 32, name: "CR");
+                .WithValueField(0, 32,
+                    valueProviderCallback: _ => crReg,
+                    writeCallback: (_, val) => crReg = (uint)val,
+                    name: "CR");
 
-            // DCR1 @ 0x008: Device configuration 1
-            // CKMODE (bit 0), FRCK (bit 1), CSHT [13:8],
-            // DEVSIZE [20:16], EXTENDMEM (bit 21), MTYP [26:24]
             Registers.DCR1.Define(this)
-                .WithValueField(0, 32, name: "DCR1");
+                .WithValueField(0, 32,
+                    valueProviderCallback: _ => dcr1Reg,
+                    writeCallback: (_, val) => dcr1Reg = (uint)val,
+                    name: "DCR1");
 
-            // DCR2 @ 0x00C: Device configuration 2
-            // PRESCALER [7:0], WRAPSIZE [18:16]
             Registers.DCR2.Define(this)
                 .WithValueField(0, 32, name: "DCR2");
 
-            // DCR3 @ 0x010: Device configuration 3
-            // MAXTRAN [7:0], CSBOUND [20:16]
             Registers.DCR3.Define(this)
                 .WithValueField(0, 32, name: "DCR3");
 
-            // DCR4 @ 0x014: Device configuration 4
-            // REFRESH [31:0]
             Registers.DCR4.Define(this)
                 .WithValueField(0, 32, name: "DCR4");
 
@@ -74,9 +88,12 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             // TEF (bit 0), TCF (bit 1), FTF (bit 2), SMF (bit 3),
             // TOF (bit 4), BUSY (bit 5), FLEVEL [14:8]
             Registers.SR.Define(this)
-                .WithFlag(0, FieldMode.Read, name: "TEF")
-                .WithFlag(1, FieldMode.Read, name: "TCF")
-                .WithFlag(2, FieldMode.Read, name: "FTF")
+                .WithFlag(0, FieldMode.Read,
+                    valueProviderCallback: _ => tef, name: "TEF")
+                .WithFlag(1, FieldMode.Read,
+                    valueProviderCallback: _ => tcf, name: "TCF")
+                .WithFlag(2, FieldMode.Read,
+                    valueProviderCallback: _ => ftf, name: "FTF")
                 .WithFlag(3, FieldMode.Read, name: "SMF")
                 .WithFlag(4, FieldMode.Read, name: "TOF")
                 .WithFlag(5, FieldMode.Read,
@@ -88,38 +105,48 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             // FCR @ 0x024: Flag clear register (write-1-to-clear)
             // CTEF (bit 0), CTCF (bit 1), CSMF (bit 3), CTOF (bit 4)
             Registers.FCR.Define(this)
-                .WithValueField(0, 32, FieldMode.Write, name: "FCR");
+                .WithValueField(0, 32, FieldMode.Write, name: "FCR",
+                    writeCallback: (_, val) =>
+                    {
+                        if ((val & 0x1) != 0)
+                        {
+                            tef = false;
+                        }
+                        if ((val & 0x2) != 0)
+                        {
+                            tcf = false;
+                        }
+                        // FTF is not cleared by FCR on HW (level flag)
+                    });
 
-            // DLR @ 0x040: Data length register
             Registers.DLR.Define(this)
                 .WithValueField(0, 32, name: "DLR");
 
-            // CCR @ 0x100: Communication configuration
-            // IMODE [2:0], IDTR (bit 3), ISIZE [5:4],
-            // ADMODE [10:8], ADDTR (bit 11), ADSIZE [13:12],
-            // ABMODE [18:16], ABDTR (bit 19), ABSIZE [21:20],
-            // DMODE [26:24], DDTR (bit 27), DQSE (bit 29)
+            // CCR write may start a transfer depending on FMODE; L2: complete
             Registers.CCR.Define(this)
-                .WithValueField(0, 32, name: "CCR");
+                .WithValueField(0, 32, name: "CCR",
+                    writeCallback: (_, __) => CompleteTransfer());
 
-            // TCR @ 0x108: Timing configuration
-            // DCYC [4:0], DHQC (bit 28), SSHIFT (bit 30)
             Registers.TCR.Define(this)
                 .WithValueField(0, 32, name: "TCR");
 
-            // IR @ 0x110: Instruction register
+            // IR write commonly triggers the command phase
             Registers.IR.Define(this)
-                .WithValueField(0, 32, name: "IR");
+                .WithValueField(0, 32, name: "IR",
+                    writeCallback: (_, __) => CompleteTransfer());
 
-            // ABR @ 0x120: Alternate bytes register
             Registers.ABR.Define(this)
                 .WithValueField(0, 32, name: "ABR");
 
-            // LPTR @ 0x130: Low-power timeout register
-            // TIMEOUT [15:0]
             Registers.LPTR.Define(this)
                 .WithValueField(0, 32, name: "LPTR");
         }
+
+        private uint crReg;
+        private uint dcr1Reg;
+        private bool tcf;
+        private bool tef;
+        private bool ftf;
 
         private enum Registers : long
         {
--- tests/renode/tests/018-xspi.robot
+++ tests/renode/tests/018-xspi.robot
@@ -1,8 +1,15 @@
 *** Settings ***
 Resource        resources/stm32n6-common.robot
+Force Tags      xspi
 
 *** Variables ***
 ${XSPI_BASE}    0x48025000
+${REG_CR}       0x00
+${REG_DCR1}     0x08
+${REG_SR}       0x20
+${REG_FCR}      0x24
+${REG_CCR}      0x100
+${REG_IR}       0x110
 
 *** Keywords ***
 Read XSPI Register
@@ -19,23 +26,59 @@ Write XSPI Register
 
 *** Test Cases ***
 XSPI1 SR Reset Value
+    [Tags]    L1-register
     Start STM32N6
-    ${val}=    Read XSPI Register    0x20
-    # SR: BUSY (bit 5) should be clear, TCF (bit 0) should be clear
+    ${val}=    Read XSPI Register    ${REG_SR}
+    # SR: BUSY (bit 5) should be clear, TCF clear
     Should Be Equal As Integers    ${val}    0
 
 XSPI1 CR Writable
+    [Tags]    L1-register
     Start STM32N6
-    Write XSPI Register    0x00    0x01
-    ${val}=    Read XSPI Register    0x00
+    Write XSPI Register    ${REG_CR}    0x01
+    ${val}=    Read XSPI Register    ${REG_CR}
     Should Be Equal As Integers    ${val}    0x01
 
 XSPI1 DCR1 Writable
+    [Tags]    L1-register
     Start STM32N6
-    Write XSPI Register    0x08    0x00FF0000
-    ${val}=    Read XSPI Register    0x08
+    Write XSPI Register    ${REG_DCR1}    0x00FF0000
+    ${val}=    Read XSPI Register    ${REG_DCR1}
     Should Be Equal As Integers    ${val}    0x00FF0000
 
+XSPI1 BUSY Always Clear
+    [Tags]    L2-state
+    Start STM32N6
+    Write XSPI Register    ${REG_CR}    0x01
+    Write XSPI Register    ${REG_IR}    0x9F
+    ${sr}=    Read XSPI Register    ${REG_SR}
+    ${sr}=    Convert To Integer    ${sr}
+    Should Be True    (${sr} & 0x20) == 0
+
+XSPI1 IR Completes With TCF
+    [Tags]    L2-state
+    Start STM32N6
+    Write XSPI Register    ${REG_CR}    0x01
+    Write XSPI Register    ${REG_IR}    0x05
+    ${sr}=    Read XSPI Register    ${REG_SR}
+    ${sr}=    Convert To Integer    ${sr}
+    # TCF bit 1
+    Should Be True    (${sr} & 0x02) == 0x02
+
+XSPI1 FCR Clears TCF
+    [Tags]    L2-state
+    Start STM32N6
+    Write XSPI Register    ${REG_CR}    0x01
+    Write XSPI Register    ${REG_CCR}    0x01
+    ${sr}=    Read XSPI Register    ${REG_SR}
+    ${sr}=    Convert To Integer    ${sr}
+    Should Be True    (${sr} & 0x02) == 0x02
+    Write XSPI Register    ${REG_FCR}    0x02
+    ${sr2}=    Read XSPI Register    ${REG_SR}
+    ${sr2}=    Convert To Integer    ${sr2}
+    Should Be True    (${sr2} & 0x02) == 0
+
 Boot Regression
+    [Tags]    boot-regression
     Start STM32N6
     Wait For NSH
