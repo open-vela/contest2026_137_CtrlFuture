1fe586f0ce039875fa1750befa1b66232c95a331
changting27 <changting27@outlook.com>
2026-07-14 11:26:01 +0800

    tests: raise RTC Renode model to L2 (ADR-018)
    Model WPR unlock (0xCA/0x53), INIT->INITF, RSF/INITS calendar
    init, and write protection for CR/PRER/TR/DR/WUTR when locked.
    
    Signed-off-by: changting27 <changting27@outlook.com>

--- tests/renode/peripherals/STM32N6_RTC.cs
+++ tests/renode/peripherals/STM32N6_RTC.cs
@@ -4,7 +4,8 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 // STM32N6 RTC model for Renode.
-// Register layout matches CMSIS RTC_TypeDef (stm32n647xx.h).
+// L2 model: WPR unlock (0xCA then 0x53), INIT->INITF, calendar
+// init sets INITS+RSF, protected CR/PRER/TR/DR writes when locked.
 //
 // Registers:
 //   TR       @ 0x00: Time register (BCD, RW in init mode)
@@ -36,15 +37,50 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
 
         public long Size => 0x400;
 
+        public override void Reset()
+        {
+            base.Reset();
+            initMode = false;
+            calendarInit = false;
+            registersSynced = false;
+            writeProtected = true;
+            unlockStep = 0;
+            timeReg = 0;
+            dateReg = 0;
+            prerReg = 0x007F00FF; // PREDIV_A=127, PREDIV_S=255 typical
+            crReg = 0;
+            wutrReg = 0xFFFF;
+        }
+
         private void DefineRegisters()
         {
             // TR @ 0x00: Time register (BCD)
             Registers.TR.Define(this)
-                .WithValueField(0, 32, name: "TR");
+                .WithValueField(0, 32,
+                    valueProviderCallback: _ => timeReg,
+                    writeCallback: (_, val) =>
+                    {
+                        if (!writeProtected && initMode)
+                        {
+                            timeReg = (uint)val;
+                            calendarInit = true;
+                        }
+                    },
+                    name: "TR");
 
             // DR @ 0x04: Date register (BCD)
             Registers.DR.Define(this)
-                .WithValueField(0, 32, name: "DR");
+                .WithValueField(0, 32,
+                    valueProviderCallback: _ => dateReg,
+                    writeCallback: (_, val) =>
+                    {
+                        if (!writeProtected && initMode)
+                        {
+                            dateReg = (uint)val;
+                            calendarInit = true;
+                        }
+                    },
+                    name: "DR");
 
             // SSR @ 0x08: Sub-second register (read-only)
             Registers.SSR.Define(this)
@@ -60,26 +96,80 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
                 .WithFlag(3, FieldMode.Read,
                     valueProviderCallback: _ => false, name: "SHPF")
                 .WithFlag(4, FieldMode.Read,
-                    valueProviderCallback: _ => false, name: "INITS")
-                .WithFlag(5, name: "RSF")
+                    valueProviderCallback: _ => calendarInit, name: "INITS")
+                .WithFlag(5,
+                    valueProviderCallback: _ => registersSynced,
+                    writeCallback: (_, val) =>
+                    {
+                        // Hardware: software clears RSF by writing 0
+                        if (!val)
+                        {
+                            registersSynced = false;
+                        }
+                    },
+                    name: "RSF")
                 .WithFlag(6, FieldMode.Read,
                     valueProviderCallback: _ => initMode, name: "INITF")
                 .WithFlag(7, name: "INIT",
-                    writeCallback: (_, val) => initMode = val,
+                    writeCallback: (_, val) =>
+                    {
+                        if (writeProtected)
+                        {
+                            return;
+                        }
+                        if (val)
+                        {
+                            initMode = true;
+                            registersSynced = false;
+                        }
+                        else
+                        {
+                            // Exit init: INITF clears, RSF sets when calendar ready
+                            initMode = false;
+                            registersSynced = true;
+                        }
+                    },
                     valueProviderCallback: _ => initMode)
                 .WithReservedBits(8, 24);
 
             // PRER @ 0x10: Prescaler register
             Registers.PRER.Define(this)
-                .WithValueField(0, 32, name: "PRER");
+                .WithValueField(0, 32,
+                    valueProviderCallback: _ => prerReg,
+                    writeCallback: (_, val) =>
+                    {
+                        if (!writeProtected)
+                        {
+                            prerReg = (uint)val;
+                        }
+                    },
+                    name: "PRER");
 
             // WUTR @ 0x14: Wakeup timer register
             Registers.WUTR.Define(this)
-                .WithValueField(0, 32, name: "WUTR");
+                .WithValueField(0, 32,
+                    valueProviderCallback: _ => wutrReg,
+                    writeCallback: (_, val) =>
+                    {
+                        if (!writeProtected)
+                        {
+                            wutrReg = (uint)val;
+                        }
+                    },
+                    name: "WUTR");
 
             // CR @ 0x18: Control register
             Registers.CR.Define(this)
-                .WithValueField(0, 32, name: "CR");
+                .WithValueField(0, 32,
+                    valueProviderCallback: _ => crReg,
+                    writeCallback: (_, val) =>
+                    {
+                        if (!writeProtected)
+                        {
+                            crReg = (uint)val;
+                        }
+                    },
+                    name: "CR");
 
             // PRIVCFGR @ 0x1C: Privilege configuration
             Registers.PRIVCFGR.Define(this)
@@ -90,8 +180,26 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
                 .WithValueField(0, 32, name: "SECCFGR");
 
             // WPR @ 0x24: Write protection (write-only)
+            // Unlock sequence: write 0xCA then 0x53. Any other value re-locks.
             Registers.WPR.Define(this)
-                .WithValueField(0, 32, FieldMode.Write, name: "WPR");
+                .WithValueField(0, 8, FieldMode.Write, name: "WPR",
+                    writeCallback: (_, val) =>
+                    {
+                        if (val == 0xCA && unlockStep == 0)
+                        {
+                            unlockStep = 1;
+                        }
+                        else if (val == 0x53 && unlockStep == 1)
+                        {
+                            writeProtected = false;
+                            unlockStep = 0;
+                        }
+                        else
+                        {
+                            writeProtected = true;
+                            unlockStep = 0;
+                        }
+                    });
 
             // SR @ 0x50: Status register
             // ALRAF (bit 0), ALRBF (bit 1), WUTF (bit 2),
@@ -108,6 +216,15 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
         }
 
         private bool initMode;
+        private bool calendarInit;
+        private bool registersSynced;
+        private bool writeProtected = true;
+        private int unlockStep;
+        private uint timeReg;
+        private uint dateReg;
+        private uint prerReg = 0x007F00FF;
+        private uint crReg;
+        private uint wutrReg = 0xFFFF;
 
         private enum Registers : long
         {
--- tests/renode/tests/016-rtc.robot
+++ tests/renode/tests/016-rtc.robot
@@ -1,8 +1,15 @@
 *** Settings ***
 Resource        resources/stm32n6-common.robot
+Force Tags      rtc
 
 *** Variables ***
 ${RTC_BASE}     0x46004000
+${REG_TR}       0x00
+${REG_DR}       0x04
+${REG_ICSR}     0x0C
+${REG_PRER}     0x10
+${REG_CR}       0x18
+${REG_WPR}      0x24
 
 *** Keywords ***
 Read RTC Register
@@ -17,27 +24,83 @@ Write RTC Register
     ${addr}=    Evaluate    ${RTC_BASE} + ${offset}
     Execute Command    sysbus WriteDoubleWord ${addr} ${value}
 
+Unlock RTC Write Protection
+    Write RTC Register    ${REG_WPR}    0xCA
+    Write RTC Register    ${REG_WPR}    0x53
+
 *** Test Cases ***
 RTC ICSR Reset Value
+    [Tags]    L1-register
     Start STM32N6
-    ${val}=    Read RTC Register    0x0C
+    ${val}=    Read RTC Register    ${REG_ICSR}
     # ICSR reset: WUTWF (bit 2) set
     Should Be Equal As Integers    ${val}    0x04
 
-RTC Init Mode Entry
-    Start STM32N6
-    # Set INIT bit (bit 7) to enter init mode
-    Write RTC Register    0x0C    0x87
-    ${isr}=    Read RTC Register    0x0C
-    # INITF (bit 6) should be set
-    ${initf}=    Evaluate    (${isr} >> 6) & 1
-    Should Be Equal As Integers    ${initf}    1
-
 RTC Time Register Accessible
+    [Tags]    L1-register
     Start STM32N6
-    ${val}=    Read RTC Register    0x00
+    ${val}=    Read RTC Register    ${REG_TR}
     Should Be True    int(${val}) >= 0
 
+RTC Init Mode Entry Requires Unlock
+    [Tags]    L2-state
+    Start STM32N6
+    # Without unlock, INIT write is ignored
+    Write RTC Register    ${REG_ICSR}    0x80
+    ${isr}=    Read RTC Register    ${REG_ICSR}
+    ${isr}=    Convert To Integer    ${isr}
+    Should Be True    (${isr} & 0xC0) == 0
+    # Unlock then set INIT
+    Unlock RTC Write Protection
+    Write RTC Register    ${REG_ICSR}    0x80
+    ${isr2}=    Read RTC Register    ${REG_ICSR}
+    ${isr2}=    Convert To Integer    ${isr2}
+    # INITF (bit 6) and INIT (bit 7) set
+    Should Be True    (${isr2} & 0xC0) == 0xC0
+
+RTC Init Exit Sets RSF And INITS
+    [Tags]    L2-state
+    Start STM32N6
+    Unlock RTC Write Protection
+    Write RTC Register    ${REG_ICSR}    0x80
+    # Program calendar in init mode
+    Write RTC Register    ${REG_TR}    0x00123000
+    Write RTC Register    ${REG_DR}    0x00240101
+    # Exit init mode
+    Write RTC Register    ${REG_ICSR}    0x00
+    ${isr}=    Read RTC Register    ${REG_ICSR}
+    ${isr}=    Convert To Integer    ${isr}
+    # INITF clear, RSF (bit5) and INITS (bit4) set, WUTWF (bit2)
+    Should Be True    (${isr} & 0x40) == 0
+    Should Be True    (${isr} & 0x20) == 0x20
+    Should Be True    (${isr} & 0x10) == 0x10
+    Should Be True    (${isr} & 0x04) == 0x04
+    ${tr}=    Read RTC Register    ${REG_TR}
+    Should Be Equal As Integers    ${tr}    0x00123000
+
+RTC CR Protected Until Unlock
+    [Tags]    L2-state
+    Start STM32N6
+    Write RTC Register    ${REG_CR}    0x400
+    ${cr}=    Read RTC Register    ${REG_CR}
+    Should Be Equal As Integers    ${cr}    0
+    Unlock RTC Write Protection
+    Write RTC Register    ${REG_CR}    0x400
+    ${cr2}=    Read RTC Register    ${REG_CR}
+    Should Be Equal As Integers    ${cr2}    0x400
+
+RTC WPR Relock
+    [Tags]    L2-state
+    Start STM32N6
+    Unlock RTC Write Protection
+    Write RTC Register    ${REG_CR}    0x100
+    # Any non-sequence key re-locks
+    Write RTC Register    ${REG_WPR}    0xFF
+    Write RTC Register    ${REG_CR}    0x200
+    ${cr}=    Read RTC Register    ${REG_CR}
+    Should Be Equal As Integers    ${cr}    0x100
+
 Boot Regression
+    [Tags]    boot-regression
     Start STM32N6
     Wait For NSH
